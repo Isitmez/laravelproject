4840cb8885dd7596a366b0ebeb2e4b4065862c81
Isitmez <isitmezeren3@gmail.com>
2026-06-11 15:47:30 +0300

    Unwanted uselles comments gone

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -16,9 +16,6 @@ class Setting extends Model
         return $setting ? $setting->value : $default;
     }
 
-    /**
-     * Set a setting value by key.
-     */
     public static function set(string $key, mixed $value): void
     {
         static::updateOrCreate(['key' => $key], ['value' => $value]);
