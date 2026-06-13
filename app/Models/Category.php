52c15c8e0397c211c4dee4cb8eee4c083af71b0d
Isitmez <isitmezeren3@gmail.com>
2026-06-13 23:12:41 +0300

    docs(category): add PHPDoc comments to Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * Class Category
+ *
+ * @package App\Models
+ * @property int $id
+ * @property int $parent_id
+ * @property string $title
+ * @property string|null $keywords
+ * @property string|null $description
+ * @property string|null $image
+ * @property bool $status
+ * @property-read \Illuminate\Database\Eloquent\Collection|Category[] $children
+ * @property-read Category|null $parent
+ * @property-read \Illuminate\Database\Eloquent\Collection|Product[] $products
+ */
 class Category extends Model
 {
     protected $fillable = [
